2d24ffff73cbcf15c88318cbaa4307dfe7625c15
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-20 18:49:24 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -414,7 +414,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ".format(f"{EMOJI_HELP}", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} {} ".format(f"{EMOJI_HELP}", x, f"{EMOJI_HELP}"), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -432,7 +432,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "< ̤< ̤", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "🌟𝗖𝗟𝗢𝗦𝗘🌟", data="{}_close({})".format(prefix, modulo_page)
+                    f"{EMOJI_HELP}𝗖𝗟𝗢𝗦𝗘{EMOJI_HELP}", data="{}_close({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
                     "> ̤> ̤", data="{}_next({})".format(prefix, modulo_page)
